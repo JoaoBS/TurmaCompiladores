b6460850a4ec88f21f1bd6ab0b856822fded39cc
Joao <joaobsneto2002@gmail.com>
2024-03-19 11:03:57 -0300

    atividade compiladores

---
+++ main.py
@@ -0,0 +1,85 @@
+from flask import *
+import dao
+
+#isntancia o servidor flask
+app = Flask(__name__)
+app.secret_key = '3j45h3j2k4h5kj3h45h23JHGJHgh'
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/inscricao')
+def inscricao():
+    return render_template('inscricao.html')
+
+@app.route('/logado')
+def logado():
+    return render_template('logado.html')
+
+@app.route('/cadastrarusuario', methods=['POST'])
+def cadastrar_usuario():
+
+    nome = request.form.get('nomeusuario')
+    idade = request.form.get('idadeusuario')
+    email = request.form.get('emailusuario')
+    senha = request.form.get('senhausuario')
+
+    if dao.cadastrarusuario(nome, idade, email, senha):
+        return render_template('home.html', msg='Usuário inserido com sucesso')
+    else:
+        return render_template('home.html', msg='Usuário já existe')
+
+
+@app.route('/verificarlogin', methods=['POST'])
+def verificar_login():
+    user = request.form.get('emailusuario')
+    senha = request.form.get('senhausuario')
+
+    if dao.checarlogin(user, senha):
+        session['idusuario'] = user
+        return render_template('logado.html', email=user)
+    else:
+        return render_template('errologin.html')
+
+
+@app.route('/entraremcontato')
+def mostrarpaginacontato():
+    if session.get('idusuario') != None:
+        return render_template('contato.html', email=session['idusuario'])
+    else:
+        return render_template('home.html')
+
+@app.route('/inserircontato', methods=['POST','GET'])
+def inserircontato():
+
+    if request.method == 'POST':
+        nome = request.form.get('nome')  # POST
+        email = request.form.get('email')
+        texto = request.form.get('texto')
+        cep = request.form.get('cep')
+    else:
+        nome = request.args.get('nome')  # GET
+        email = request.args.get('email')
+        texto = request.args.get('texto')
+        cep = request.args.get('cep')
+
+    cep = cep.replace('-', '')
+    if dao.registrar_contato(nome, email, texto, cep):
+        return render_template('logado.html', msg='Comentário enviado')
+    else:
+        #criar pagina de erro de contato
+        return render_template('errocontato.html', msg='Não foi possível enviar comentário')
+
+
+@app.route('/exibirusuarios')
+def exibirUsuarios():
+
+    headings = ('Nome', 'Idade', 'E-mail')
+    dados = dao.listarUsuarios()
+
+    return render_template('tabelausuarios.html', headings=headings, data=dados)
+
+
+if __name__ == '__main__':
+    app.run(debug=True) #executa/roda/starta o servidor
